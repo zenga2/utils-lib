1fd8a4335b1d4d2141c19b09cb059a459172857e
zenga2 <2459672025@qq.com>
2017-04-22 16:04:07 +0800

    add  function setCss

--- src-es6/util/pageUtils.js
+++ src-es6/util/pageUtils.js
@@ -1,15 +1,24 @@
 import {each} from './utils'
 
-// 给元素增加内联样式
+// 给元素设置内联样式
 // todo 考虑前缀的情况
-function insertStyle(el, styleObj) {
+function setCss(el, styleObj) {
     if (!el) {
         throw new Error('invalid argument: el cannot be empty')
     } else if (typeof el === 'string') {
         el = document.querySelector(el)
     }
 
+    var bodyStyleObj = document.body.style;
+
     each(styleObj, function (val, prop) {
+        // 判断是否需要加前缀(这里针对的是移动端，所以只考虑webkit)
+        var wProp = 'webkit' + firstLetterToUpperCase(prop)
+
+        if (!(prop in bodyStyleObj) && (wProp in bodyStyleObj)) {
+            prop = wProp
+        }
+
         el.style[prop] = val
     })
 }
